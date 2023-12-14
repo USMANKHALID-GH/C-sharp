0ac9b2c3f3174b324e2585c00a76c7c7312cb629
USMANKHALID-GH <loftyusman@gmail.com>
2023-12-14 23:04:00 +0300

    oop

--- oop_c#/oop_c#/Program.cs
+++ oop_c#/oop_c#/Program.cs
@@ -10,6 +10,66 @@ namespace oop_c_
     {
         static void Main(string[] args)
         {
+            Student student = new Student("usman", "khalid", 'M', 45, "Black");
+            Console.WriteLine(student.ToString());
+        }
+    }
+
+    class  Student
+    {
+        private string firstName;
+        private string secondName;
+        private char sex;
+        private int age;
+        private string hairColor;
+
+        public Student(string firstName, string secondName, char sex, int age, string hairColor)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.sex = sex;
+            this.age = age;
+            this.hairColor = hairColor;
+        }
+
+        public Student()
+        {
+            
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set => firstName = value;
+        }
+
+        public string SecondName
+        {
+            get => secondName;
+            set => secondName = value;
+        }
+
+        public char Sex
+        {
+            get => sex;
+            set => sex = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public string HairColor
+        {
+            get => hairColor;
+            set => hairColor = value;
+        }
+
+        public override string ToString()
+        {
+            return $"First Name: {firstName}  Last Name: {SecondName}   Sex: {sex} Hair Color: {hairColor}  Age: {age}";
         }
     }
 }
