70403537c267ba0d9a14e80f93707c0da2afcf60
USMANKHALID-GH <loftyusman@gmail.com>
2023-12-16 23:11:11 +0300

    datastructure

---
+++ oop_c#/datastructure/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace datastructure
+{
+   
+    
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(3);
+            arrayList.Add(4);
+            arrayList.Add(5);
+            arrayList.Add(6);
+            arrayList.Add(7);
+            arrayList.Add(8);
+            arrayList.Add(9);
+            arrayList.Add(10);
+            Console.WriteLine(arrayList.ToString());
+        }
+    }
+}
