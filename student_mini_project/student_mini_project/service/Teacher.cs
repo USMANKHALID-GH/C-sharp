e00752087d1f282af0909c86127254aeffb3884a
USMANKHALID-GH <loftyusman@gmail.com>
2024-01-01 18:26:52 +0300

    Teacher interface

---
+++ student_mini_project/student_mini_project/service/Teacher.cs
@@ -0,0 +1,12 @@
+﻿namespace MainProject.service;
+using MainProject.model;
+
+public interface TeacherService
+{
+    void saveTeacher(Teacher teacher);
+    Teacher getTeacherById(int id);
+    List<Teacher> getAllTeachers();
+    void updateTeacher(Teacher teacher, int id);
+    void deleteTeacher(int id);
+    
+}
