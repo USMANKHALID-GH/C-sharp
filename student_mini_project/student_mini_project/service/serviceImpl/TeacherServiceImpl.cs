aedf6bdecb82c069e1ef0c387ce5f3374fbb7cdf
USMANKHALID-GH <loftyusman@gmail.com>
2024-01-01 18:27:03 +0300

    Teacher implementation

---
+++ student_mini_project/student_mini_project/service/serviceImpl/TeacherServiceImpl.cs
@@ -0,0 +1,42 @@
+﻿namespace MainProject.service.serviceImpl;
+using MainProject.model;
+public class TeacherServiceImpl: TeacherService
+{
+    
+    private List<Teacher> _teachers = new();
+    
+    
+    public void saveTeacher(Teacher teacher)
+    {
+        
+        _teachers.Add(teacher);
+    }
+    
+    public Teacher getTeacherById(int id)
+    {
+        return GetById(id);
+    }
+    
+    public List<Teacher> getAllTeachers()
+    {
+        return _teachers;
+    }
+    
+    public void updateTeacher(Teacher teacher, int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void deleteTeacher(int id)
+    {
+        Teacher teacher = GetById(id);
+       if (teacher.Courses == null) throw new Exception("Teacher Cant be Deleted" + id); 
+        _teachers.Remove(teacher);
+    }
+    
+    private Teacher GetById(int id)
+    {
+       return _teachers.Where(d => d.Id == id).FirstOrDefault() ?? throw new Exception("Teacher not found" + id);
+    }
+    
+}
